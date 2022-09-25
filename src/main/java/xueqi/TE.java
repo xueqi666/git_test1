89b0dfb3ec103a65c6d34f7bca6175cc86f39abd
xueqi <xueqi@qq.com>
2022-09-25 22:55:52 +0800

    updata .gitignore

---
+++ src/main/java/xueqi/TE.java
@@ -0,0 +1,15 @@
+package xueqi;
+
+/**
+ * @author xueqi
+ * @create 2022-24-26 19:24
+ */
+public class TE {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello word");
+    }
+
+
+}
