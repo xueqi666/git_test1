b9679a209b7c601f3c283664af8f0906e128f678
xueqi <xueqi@qq.com>
2022-09-26 19:32:25 +0800

    updata .gitignore

--- src/main/java/xueqi/TE.java
+++ src/main/java/xueqi/TE.java
@@ -9,6 +9,8 @@ public class TE {
     public static void main(String[] args) {
 
         System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
     }
 
 
